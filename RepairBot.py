a07965468bced880ef7d5ec36823719ab8f1227e
cartographer1 <167234854+cartographer1@users.noreply.github.com>
2024-09-07 12:46:25 +0800

    Update RepairBot.py

--- RepairBot.py
+++ RepairBot.py
@@ -3,5 +3,7 @@ def RepairBot(target):
     SetToComponent(target, "c_repairer")
     if CheckHealth(target):
       WaitTicks(20)
+    else:
+      SetToComponent(A, '"c_repairer"')
 
   WaitTicks(20)
